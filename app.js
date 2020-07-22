459911e7ceaa859400d1f16fb0478a3fc55dd215
Tiffany Gray <ttgray@deakin.edu.au>
2020-07-22 21:03:01 +0930

    UPDATE to app.js, device-list and register-device

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const devices = [];
-devices.push({ user: "Mary", name: "Mary's iPhone" });
-devices.push({ user: "Alex", name: "Alex's Surface Pro" });
-devices.push({ user: "Mary", name: "Mary's MacBook" });
+//const devices = [];
+//devices.push({ user: "Mary", name: "Mary's iPhone" });
+//devices.push({ user: "Alex", name: "Alex's Surface Pro" });
+//devices.push({ user: "Mary", name: "Mary's MacBook" });
 
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
 devices.forEach(function(device) {
 	$('#devices tbody').append(`
@@ -12,11 +13,17 @@ devices.forEach(function(device) {
 		</tr>`
 	);
 });
+
+
 $('#add-device').on('click', function() {
 	const user = $('#user').val();
 	const name = $('#name').val();
-	devices.push({ user, name });
-	console.log(devices);
+	devices.push({ user , name });
+	localStorage.setItem('devices', JSON.stringify(devices));
+	location.href = 'device-list.html';
 });
 
-
+$('#send-command').on('click', function() {
+	const command = $('#comand').val();
+	console.log(`command is: ${command}`);
+});
